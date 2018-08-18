c682ca6cc6be20046a12fd5483b6d05a1744f6cb
Dylan McClung <dylanmcclung@gmail.com>
2018-08-18 21:38:07 +0000

    Finished up example of chart reporting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const output = require("d3node-output");
 const d3 = require("d3-node")().d3;
-const d3linechart = require("d3node-linechart");
+const d3nLine = require("d3node-linechart");
 
-const parseTime = d3.timeParse("%d-%b-%y");
+const parseTime = d3.timeParse("%y");
 const tsv = fs.readFileSync("647_Global_Temperature_Data_File.txt").toString();
 const data = d3.tsvParse(tsv, d => {
   return {
-    key: parseTime(d.date),
-    value: +d.close
+    key: d.year,
+    value: +d.annual
   };
 });
 
-output('./example/output', d3linechart({ data: data }));
+output('output', d3nLine({ data: data }));
